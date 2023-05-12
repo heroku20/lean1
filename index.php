31c43267f6deda2855267f0e95930c6a50bd30b6
heroku20 <130670798+heroku20@users.noreply.github.com>
2023-05-12 14:15:56 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,185 @@
+ <?php
+echo '
+<!DOCTYPE html>
+<html  >
+<head>
+  <!-- Site made with Mobirise Website Builder v5.8.9, https://mobirise.com -->
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="generator" content="Mobirise v5.8.9, mobirise.com">
+  <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+  <link rel="shortcut icon" href="assets/images/logo5.png" type="image/x-icon">
+  <meta name="description" content="">
+  
+  
+  <title>LeanBiome</title>
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-grid.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-reboot.min.css">
+  <link rel="stylesheet" href="assets/socicon/css/styles.css">
+  <link rel="stylesheet" href="assets/theme/css/style.css">
+  <link rel="preload" href="https://fonts.googleapis.com/css?family=Jost:100,200,300,400,500,600,700,800,900,100i,200i,300i,400i,500i,600i,700i,800i,900i&display=swap" as="style" onload="this.onload=null;this.rel=\'stylesheet\'">
+  <noscript><link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Jost:100,200,300,400,500,600,700,800,900,100i,200i,300i,400i,500i,600i,700i,800i,900i&display=swap"></noscript>
+  <link rel="preload" as="style" href="assets/mobirise/css/mbr-additional.css"><link rel="stylesheet" href="assets/mobirise/css/mbr-additional.css" type="text/css">
+
+  
+  
+  
+</head>
+<body>
+  
+  <section data-bs-version="5.1" class="features6 cid-tDE7Vzr7ap" id="features7-1g">
+    <!---->
+    
+
+    
+    
+    <div class="container">
+        <div class="card-wrapper">
+            <div class="row align-items-center">
+                <div class="col-12 col-lg-4">
+                    <div class="image-wrapper">
+                        <a href="https://bestofferhere.link/leanbiome"><img src="assets/images/jj.webp" alt="LeanBiome"></a>
+                    </div>
+                </div>
+                <div class="col-12 col-lg">
+                    <div class="text-box">
+                        <h5 class="mbr-title mbr-fonts-style display-2">
+                            <strong>LeanBiome™ Official</strong></h5>
+                        <p class="mbr-text mbr-fonts-style display-7">The Formula is Easy to Take Each Day, and it Only Uses Natural Ingredients to Get the Desired Effect.<br><br><strong>Special Offer Save Up to 70% OFF + Free Shipping!<br>Only for: $39/per bottle</strong><br><br></p>
+                        <div class="cost">
+                            <span class="currentcost mbr-fonts-style pr-2 display-2">$39</span>
+                            <span class="oldcost mbr-fonts-style display-2">$129</span>
+                        </div>
+                        <div class="mbr-section-btn pt-3"><a href="https://bestofferhere.link/leanbiome" class="btn btn-secondary display-4">Buy Now</a></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section data-bs-version="5.1" class="content15 cid-tDDLX1r0CN" id="content15-1c">
+
+    
+
+    
+    
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="card col-md-12 col-lg-10">
+                <div class="card-wrapper">
+                    <div class="card-box align-left">
+                        <h4 class="card-title mbr-fonts-style mbr-white mb-3 display-5"><strong>180-Day "Empty Bottle"&nbsp;</strong><strong>Money-Back Guarantee</strong></h4>
+                        <p class="mbr-text mbr-fonts-style display-7">I\'m so confident you\'ll be head-over-heels thrilled with the results you get that I\'m giving you a full six months to try LeanBiome for yourself, completely risk-free, regardless of the package you choose.
+<br>
+<br>If you change your mind for any reason at all, simply contact my friendly customer support team, and they\'ll refund 100% of the purchase price, no questions asked.
+<br>
+<br>Contact details will be on the packaging.</p>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section data-bs-version="5.1" class="pricing2 cid-tDyP0MuoVi" id="pricing2-16">
+    
+
+    
+    
+    <div class="container-fluid">
+        <div class="row justify-content-center">
+            <div class="col-12 col-md-6 align-center col-lg-4">
+                <div class="plan">
+                    <div class="plan-header">
+                        <h6 class="plan-title mbr-fonts-style mb-3 display-5"><strong>1 Bottle</strong></h6>
+                        <div class="plan-price">
+                            <p class="price mbr-fonts-style m-0 display-1"><strong>$59</strong></p>
+                            <p class="price-term mbr-fonts-style mb-3 display-7"><strong>Per Bottle</strong></p>
+                        </div>
+                    </div>
+                    <div class="plan-body">
+                        <div class="plan-list mb-4">
+                            <ul class="list-group mbr-fonts-style list-group-flush display-7">
+                                <li class="list-group-item">1-Month Supply</li><li class="list-group-item">Before Price : $129&nbsp;</li><li class="list-group-item">Now : $59</li><li class="list-group-item">You save $70.00</li><li class="list-group-item">180 Days Guarantee</li><li class="list-group-item">+$19.95 Indian Shipping Fee</li>
+                            </ul>
+                        </div>
+                        <div class="mbr-section-btn text-center"><a href="https://bestofferhere.link/leanbiome" class="btn btn-secondary display-4">Buy Now</a></div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-md-6 align-center col-lg-4">
+                <div class="plan">
+                    <div class="plan-header">
+                        <h6 class="plan-title mbr-fonts-style mb-3 display-5"><strong>6 Bottle</strong></h6>
+                        <div class="plan-price">
+                            <p class="price mbr-fonts-style m-0 display-1"><strong>$39</strong></p>
+                            <p class="price-term mbr-fonts-style mb-3 display-7"><strong>Per&nbsp;Bottle</strong></p>
+                        </div>
+                    </div>
+                    <div class="plan-body">
+                        <div class="plan-list mb-4">
+                            <ul class="list-group mbr-fonts-style list-group-flush display-7">
+                                <li class="list-group-item">6-Month Supply + Bonus</li><li class="list-group-item">Before Price : $774</li><li class="list-group-item">Now : $234</li><li class="list-group-item">You save $540.00</li><li class="list-group-item">180 Days Guarantee</li><li class="list-group-item">Free Shipping</li>
+                            </ul>
+                        </div>
+                        <div class="mbr-section-btn text-center"><a href="https://bestofferhere.link/leanbiome" class="btn btn-secondary display-4">Buy Now</a></div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-md-6 align-center col-lg-4">
+                <div class="plan">
+                    <div class="plan-header">
+                        <h6 class="plan-title mbr-fonts-style mb-3 display-5"><strong>3 Bottle</strong></h6>
+                        <div class="plan-price">
+                            <p class="price mbr-fonts-style m-0 display-1"><strong>$49</strong></p>
+                            <p class="price-term mbr-fonts-style mb-3 display-7"><strong>One&nbsp;Bottle</strong></p>
+                        </div>
+                    </div>
+                    <div class="plan-body">
+                        <div class="plan-list mb-4">
+                            <ul class="list-group mbr-fonts-style list-group-flush display-7">
+                                <li class="list-group-item">3-Month Supply + Bonus</li><li class="list-group-item">Before Price : $387</li><li class="list-group-item">Now : $147</li><li class="list-group-item">You save $240.00</li><li class="list-group-item">180 Days Guarantee</li><li class="list-group-item">+$19.95 Indian Shipping Fee</li>
+                            </ul>
+                        </div>
+                        <div class="mbr-section-btn text-center"><a href="https://bestofferhere.link/leanbiome" class="btn btn-secondary display-4">Buy Now</a></div>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+    </div>
+</section>
+
+<section data-bs-version="5.1" class="footer3 cid-s48P1Icc8J" once="footers" id="footer3-i">
+
+    
+
+    
+
+    <div class="container">
+        <div class="media-container-row align-center mbr-white">
+            <div class="row row-links">
+                <ul class="foot-menu">
+                    
+                    
+                    
+                    
+                    
+                <li class="foot-menu-item mbr-fonts-style display-7">Terms &amp; Conditions</li><li class="foot-menu-item mbr-fonts-style display-7">Privacy</li><li class="foot-menu-item mbr-fonts-style display-7">Disclaimer&nbsp;</li></ul>
+            </div>
+            
+            <div class="row row-copirayt">
+                <p class="mbr-text mb-0 mbr-fonts-style mbr-white align-center display-7">COPYRIGHT © LeanBiome - ALL RIGHTS RESERVED</p>
+            </div>
+        </div>
+    </div>
+</section><section class="display-7" style="padding: 0;align-items: center;justify-content: center;flex-wrap: wrap;    align-content: center;display: flex;position: relative;height: 4rem;"><a href="https://mobiri.se/2977113" style="flex: 1 1;height: 4rem;position: absolute;width: 100%;z-index: 1;"><img alt="" style="height: 4rem;" src="data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw=="></a><p style="margin: 0;text-align: center;" class="display-7">Made with &#8204;</p><a style="z-index:1" href="https://mobirise.com/builder/landing-page-software.html">Landing Page Software</a></section><script src="assets/bootstrap/js/bootstrap.bundle.min.js"></script>  <script src="assets/smoothscroll/smooth-scroll.js"></script>  <script src="assets/ytplayer/index.js"></script>  <script src="assets/theme/js/script.js"></script>  
+  
+  
+</body>
+</html>
+';
+?>
